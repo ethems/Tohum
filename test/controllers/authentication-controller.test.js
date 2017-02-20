0a4c7d61e4bb4a2ace1ab84054186b883e814a6a
furkano <Furkan.Ozdeslik@confirmit.com>
2017-02-20 06:35:27 -0800

    update code 2

---
+++ test/controllers/authentication-controller.test.js
@@ -0,0 +1,13 @@
+const should = require('should');
+const authenticationController = require('../../controllers/authentication-controller');
+const co = require('co');
+const sinon = require('sinon');
+const http = require('http');
+
+describe('AUTHENTICATION CONTROLLER', () => {
+  describe('Signup', () => {
+    it('Should return 400 error when request has no email and password', (done) => {
+      done();
+    });
+  });
+});
