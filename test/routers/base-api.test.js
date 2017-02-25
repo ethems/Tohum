6c5d8fac44fa7057094aeac05b96968b9b630fa7
furkano <Furkan.Ozdeslik@confirmit.com>
2017-02-25 10:10:02 -0800

    update code

--- test/routers/base-api.test.js
+++ test/routers/base-api.test.js
@@ -1,31 +1,28 @@
 const request = require('supertest');
 const mockery = require('mockery');
 const should = require('should');
+const mockgoose = require('mockgoose');
+const mongoose = require('mongoose');
+const User = require('../../models/user');
 
 describe('BASE API', () => {
   let app;
   before((done) => {
-    mockery.enable({warnOnUnregistered: false});
-    mockery.registerMock('../../controllers/authentication-controller', {
-      signup: (req, res, next) => {
-        return res.sendStatus(200);
-      },
-      signin: (req, res, next) => {
-        return res.sendStatus(200);
-      }
-    });
+    mockgoose(mongoose);
+    User.create({email: 'base@api.com', password: 'xxxxx'});
     app = require('../../app');
     done();
   });
   after((done) => {
-    mockery.disable();
-    app.close();
-    done();
+    mockgoose.reset(function() {
+      app.close();
+      done()
+    });
   });
   describe('Signup', () => {
     it('Should return 200 when POST "/api/signup"', (done) => {
       const u = {
-        email: 'aa@aa.com',
+        email: 'base1@api.com',
         password: '111'
       }
       request(app).post('/api/signup').send(u).expect(200).end((err, res) => {
